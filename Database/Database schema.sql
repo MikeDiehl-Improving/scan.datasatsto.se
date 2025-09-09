c3d5caf656aed659ed514d1cbcea5eb2f2c2c983
Daniel Hutmacher <daniel@strd.co>
2025-09-09 10:31:25 +0200

    Updated database schema and procs

--- Database/Database schema.sql
+++ Database/Database schema.sql
@@ -17,6 +17,12 @@ CREATE TABLE Scan.Identities (
     EventID     int NOT NULL,
     ID          bigint NOT NULL,
     Created     datetime2(3) NOT NULL,
+	[Name]      varbinary(250) NULL,
+	[Description] varbinary(500) NULL,
+	JobTitle    varbinary(200) NULL,
+	Email       varbinary(200) NULL,
+	Phone       varbinary(50) NULL,
+	[Location]  varbinary(200) NULL
     CONSTRAINT PK_Scan_Identities PRIMARY KEY CLUSTERED (ID),
     CONSTRAINT FK_Scan_Identities_Events FOREIGN KEY (EventID) REFERENCES Scan.Events (EventID)
 );
@@ -201,6 +207,42 @@ ORDER BY s.Scanned;
 GO
 
 -------------------------------------------------------------------------------
+--- List all of the identities associated with an event
+---
+--- 1. If provided with an encryption key, only identities with names are returned
+--- 2. If no encryption key is provided, all items are provided, and the caller
+---    will need to assign names to the identities.
+---
+-------------------------------------------------------------------------------
+
+CREATE OR ALTER PROCEDURE Scan.Get_Identities
+    @EventSecret        uniqueidentifier,
+    @EncryptionKey      nvarchar(200)=NULL
+AS
+
+SELECT (
+    SELECT TOP (1)
+        e.EventID AS eventId,
+        e.Event AS eventName,
+        (SELECT i.ID AS id,
+                CONVERT(nvarchar(250), DECRYPTBYPASSPHRASE(@EncryptionKey, [Name])) AS [name],
+                CONVERT(nvarchar(500), DECRYPTBYPASSPHRASE(@EncryptionKey, [Description])) AS [description],
+                CONVERT(nvarchar(200), DECRYPTBYPASSPHRASE(@EncryptionKey, JobTitle)) AS jobTitle,
+                CONVERT(nvarchar(50),  DECRYPTBYPASSPHRASE(@EncryptionKey, Phone)) AS phone,
+                CONVERT(nvarchar(200), DECRYPTBYPASSPHRASE(@EncryptionKey, Email)) AS email,
+                CONVERT(nvarchar(200), DECRYPTBYPASSPHRASE(@EncryptionKey, [Location])) AS [location]
+            FROM Scan.Identities AS i
+            WHERE i.EventID=e.EventID
+            --AND (@EncryptionKey IS NOT NULL AND [Name] IS NOT NULL OR @EncryptionKey IS NULL)
+            ORDER BY [name]
+            FOR JSON PATH) AS identities
+    FROM Scan.Events AS e
+    WHERE e.EventSecret=@EventSecret
+    FOR JSON PATH, WITHOUT_ARRAY_WRAPPER) AS blob;
+
+GO
+
+-------------------------------------------------------------------------------
 --- Fetch a random scans for an event
 -------------------------------------------------------------------------------
 
@@ -261,5 +303,108 @@ BEGIN TRANSACTION;
     WHERE e.Expires<@today;
 
 COMMIT TRANSACTION;
+
+GO
+
+-------------------------------------------------------------------------------
+---
+--- Applies names and descriptions to identities for an event. An optional
+--- encryption key can be applied using the @EncryptionKey parameter.
+---
+--- The JSON blob should look like this:
+--- [{ "id": 123456, "name": "Firstname Lastname", "description: "Company" }, ...]
+---
+--- Valid attributes are:
+---
+--- * id: the unique identity (int)
+--- * name: nvarchar(200)
+--- * description: nvarchar(400)
+--- * jobTitle: nvarchar(150)
+--- * phone: nvarchar(150)
+--- * email: nvarchar(150)
+--- * location: nvarchar(150)
+--- 
+--- NB: Attributes are case sensitive.
+---
+-------------------------------------------------------------------------------
+
+CREATE OR ALTER PROCEDURE Scan.Update_Identities
+    @EventSecret        uniqueidentifier,
+    @EncryptionKey      nvarchar(200)=N'',
+    @Identities_blob    nvarchar(max)
+AS
+
+DECLARE @EventID int=(SELECT EventID FROM Scan.Events WHERE EventSecret=@EventSecret);
+
+IF (@EventID IS NULL) BEGIN;
+    THROW 50001, N'Invalid or missing event secret.', 1;
+    RETURN;
+END;
+
+--- Generate new unique IDs, just in case.
+DECLARE @idcount int=(SELECT COUNT(*) FROM STRING_SPLIT(@Identities_blob, N'{'))+1;
+
+SELECT TOP (@idcount) ROW_NUMBER() OVER (ORDER BY ID) AS _rowno, ID INTO #idents
+FROM (
+    SELECT DISTINCT CAST(10000000000.+10000000000.*RAND(CHECKSUM(NEWID())) AS bigint) AS ID
+    FROM GENERATE_SERIES(1, @idcount*2, 1)) AS sub
+WHERE ID NOT IN (SELECT ID FROM Scan.Identities);
+
+
+WITH i AS (
+    SELECT EventID, ID, [Name], [Description], JobTitle, Email, Phone, [Location], Created
+    FROM Scan.Identities
+    WHERE EventID=@EventID),
+
+j AS (
+    SELECT ROW_NUMBER() OVER (ORDER BY id) AS _rowno,
+           id AS ID,
+           ENCRYPTBYPASSPHRASE(@EncryptionKey, NULLIF([name], N'')) AS [Name],
+           ENCRYPTBYPASSPHRASE(@EncryptionKey, NULLIF([description], N'')) AS [Description],
+           ENCRYPTBYPASSPHRASE(@EncryptionKey, NULLIF(jobTitle, N'')) AS JobTitle,
+           ENCRYPTBYPASSPHRASE(@EncryptionKey, NULLIF(phone, N'')) AS Phone,
+           ENCRYPTBYPASSPHRASE(@EncryptionKey, NULLIF(LOWER(email), N'')) AS Email,
+           ENCRYPTBYPASSPHRASE(@EncryptionKey, NULLIF([location], N'')) AS [Location],
+           (CASE WHEN [id] IS NOT NULL THEN ROW_NUMBER() OVER (PARTITION BY [id] ORDER BY (SELECT NULL)) ELSE 1 END) AS _duplicate
+    FROM OPENJSON(NULLIF(@Identities_blob, N'')) WITH (
+        id              bigint          '$.id',
+        [name]          nvarchar(max)   '$.name',
+        [description]   nvarchar(max)   '$.description',
+        jobTitle        nvarchar(max)   '$.jobTitle',
+        phone           nvarchar(max)   '$.phone',
+        email           nvarchar(max)   '$.email',
+        [location]      nvarchar(max)   '$.location')),
+
+blob AS (
+    SELECT ISNULL(j.ID, id.ID) AS ID,
+           j.[Name], j.[Description],
+           j.JobTitle, j.Phone, j.Email, j.[Location]
+    FROM j
+    LEFT JOIN #idents AS id ON j._rowno=id._rowno
+    WHERE j._duplicate=1
+      AND (j.ID IS NOT NULL OR
+           j.[Name] IS NOT NULL OR
+           j.[Description] IS NOT NULL OR
+           j.JobTitle IS NOT NULL OR
+           j.Phone IS NOT NULL OR
+           j.Email IS NOT NULL OR
+           j.[Location] IS NOT NULL))
+
+MERGE INTO i
+USING blob ON (i.ID=blob.ID OR blob.ID IS NULL AND i.Email=blob.Email)
+
+WHEN NOT MATCHED BY TARGET THEN
+    INSERT (EventID, ID, [Name], [Description], JobTitle, Email, Phone, [Location], Created)
+    VALUES (@EventID, blob.ID, blob.[Name], blob.[Description], blob.JobTitle, blob.Email, blob.Phone, blob.[Location], SYSDATETIME())
+
+WHEN MATCHED THEN
+    UPDATE
+    SET i.ID=blob.ID,
+        i.[Name]=blob.[Name],
+        i.[Description]=blob.[Description],
+        i.JobTitle=blob.JobTitle,
+        i.Email=blob.Email,
+        i.Phone=blob.Phone,
+        i.[Location]=blob.[Location];
 
 GO
